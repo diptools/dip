4cf61ba290c731f3d8132ca1e42b9970df20c6b1
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-07-25 14:03:44 +0200

    add local-state example

---
+++ examples/local-state.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+use bevy_dioxus::desktop::prelude::*;
+use dioxus::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Local State".to_string(),
+            ..Default::default()
+        })
+        .add_plugin(DioxusPlugin::<EmptyGlobalState, (), ()>::new(Root))
+        .run();
+}
+
+#[allow(non_snake_case)]
+fn Root(cx: Scope) -> Element {
+    let name = use_state(&cx, || "World".to_string());
+
+    cx.render(rsx! {
+        h1 { "Hello, {name} !" }
+
+        input {
+            value: "{name}",
+            oninput: |e| {
+                name.set(String::from(&e.value));
+            },
+        }
+    })
+}
