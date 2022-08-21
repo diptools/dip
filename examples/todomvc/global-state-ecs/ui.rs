647084a5adbb645ad5d871ef734f393c5a1a3f20
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-08-21 13:55:42 +0200

    keep track of hovered todo item

--- examples/todomvc/global-state-ecs/ui.rs
+++ examples/todomvc/global-state-ecs/ui.rs
@@ -1,4 +1,5 @@
 use crate::{channel::CoreCommand, global_state::TODO_LIST};
+use bevy::ecs::prelude::*;
 use bevy_dioxus::desktop::prelude::*;
 use dioxus::prelude::*;
 
@@ -8,13 +9,10 @@ pub fn Root(cx: Scope) -> Element {
     let window = use_window::<CoreCommand, ()>(&cx);
 
     let new_todo = use_state(&cx, || "".to_string());
+    let hovered = use_state(&cx, || None::<Entity>);
 
     cx.render(rsx! {
         h1 { "todos" }
-
-        todo_list.iter().map(|todo| rsx! {
-            div { "{todo.title}" }
-        })
 
         input {
             value: "{new_todo}",
@@ -25,6 +23,37 @@ pub fn Root(cx: Scope) -> Element {
                 window.send(CoreCommand::create(&e.value));
                 new_todo.set("".to_string());
             }
+        }
+
+        ul {
+            // doesn't work with dioxus <= v0.2.4 but fix is already merged in master.
+            onmouseleave: |_| {
+                hovered.set(None);
+            },
+            todo_list.iter().map(|todo| rsx! {
+                li {
+                    onmouseover: |_| {
+                        hovered.set(Some(todo.entity));
+                    },
+                    div {
+                        "{todo.title}"
+                    }
+
+                    if let Some(entity) = hovered.get() {
+                        if entity == &todo.entity {
+                            cx.render(rsx! {
+                                div {
+                                    "hovered"
+                                }
+                            })
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                }
+            })
         }
     })
 }
