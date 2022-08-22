a01d90b759f8805ead44fc843f82d7c8c5ea93a1
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-08-22 11:55:20 +0200

    cleanup Root component

--- examples/todomvc/ui.rs
+++ examples/todomvc/ui.rs
@@ -14,107 +14,118 @@ pub fn Root(cx: Scope) -> Element {
     let hovered = use_state(&cx, || None::<Entity>);
 
     cx.render(rsx! {
-        body {
-            main {
-                style: "display: flex; flex-direction: column; align-items: center;",
-                h1 { "todos" }
+        main {
+            style: "display: flex; flex-direction: column; align-items: center;",
+            h1 { "todos" }
 
-                input {
-                    value: "{new_todo}",
-                    oninput: |e| {
-                        new_todo.set(e.value.clone());
-                    },
-                    onchange: |e| {
-                        window.send(CoreCommand::create_todo(&e.value));
-                        new_todo.set("".to_string());
+            div {
+                style: "display: flex; flex-direction: column; align-items: center;",
+                div {
+                    style: "display: flex;",
+                    label {
+                        style: "margin-right: 0.25rem;",
+                        r#for: "filter-select",
+                        "Choose a filter:"
+                    }
+                    select {
+                        value: format_args!("{:?}", settings.filter),
+                        onchange: |e| {
+                            match e.value.as_str() {
+                                "All" => { window.send(CoreCommand::filter_all()) }
+                                "Active" => { window.send(CoreCommand::filter_active()) }
+                                "Completed" => { window.send(CoreCommand::filter_completed()) }
+                                _ => {}
+                            }
+                        },
+                        option {
+                            value: "All",
+                            "All"
+                        }
+                        option {
+                            value: "Active",
+                            "Active"
+                        }
+                        option {
+                            value: "Completed",
+                            "Completed"
+                        }
                     }
                 }
 
                 ul {
-                    // doesn't work with dioxus <= v0.2.4 but fix is already merged in master.
-                    style: "max-width: 400px; width: 100vw; list-style-type: none;",
-                    onmouseleave: |_| {
-                        hovered.set(None);
-                    },
-                    todo_list.iter().map(|todo| rsx! {
-                        li {
-                            style: "display: flex; align-items: center; justify-content: space-between; background: #ddd; padding: 1rem; height: 32px;",
-                            onmouseover: |_| {
-                                hovered.set(Some(todo.entity));
+                    style: "display: flex; list-style-type: none; padding: 0;",
+                    li {
+                        button {
+                            onclick: |_| {
+                                window.send(CoreCommand::toggle_all());
                             },
-                            div { style: "display: flex; align-items: center;",
-                                div {
-                                    style: "padding-right: 1rem;",
-                                    onclick: |_| {
-                                        window.send(CoreCommand::toggle_done(&todo.entity));
-                                    },
-                                    [format_args!("{}", if todo.done_at.is_some() { "✅" } else { "❎" })],
-                                }
-                                div {
-                                    "{todo.title}"
-                                }
-                            }
-
-                            if let Some(entity) = hovered.get() {
-                                if entity == &todo.entity {
-                                    cx.render(rsx! {
-                                        button {
-                                            style: "align-self: flex-end",
-                                            onclick: |_| {
-                                                window.send(CoreCommand::remove_todo(&todo.entity));
-                                            },
-                                            "X"
-                                        }
-                                    })
-                                } else {
-                                    None
-                                }
-                            } else {
-                                None
-                            }
+                            "Toggle all"
                         }
-                    })
+                        button {
+                            onclick: |_| {
+                                window.send(CoreCommand::clear_completed());
+                            },
+                            "Clear completed"
+                        }
+                    }
                 }
             }
 
-            footer {
-                div { [format_args!("Selected: {:?}", settings.filter)] }
-                ul {
-                    style: "display: flex; list-style-type: none;",
-                    li {
-                        style: "padding: 0.25rem;",
-                        onclick: |_| {
-                            window.send(CoreCommand::filter_all());
-                        },
-                        "All"
-                    }
-                    li {
-                        style: "padding: 0.25rem;",
-                        onclick: |_| {
-                            window.send(CoreCommand::filter_active());
-                        },
-                        "Active"
-                    }
-                    li {
-                        style: "padding: 0.25rem;",
-                        onclick: |_| {
-                            window.send(CoreCommand::filter_completed());
-                        },
-                        "Completed"
-                    }
+            input {
+                value: "{new_todo}",
+                oninput: |e| {
+                    new_todo.set(e.value.clone());
+                },
+                onchange: |e| {
+                    window.send(CoreCommand::create_todo(&e.value));
+                    new_todo.set("".to_string());
                 }
-                button {
-                    onclick: |_| {
-                        window.send(CoreCommand::toggle_all());
-                    },
-                    "Toggle all"
-                }
-                button {
-                    onclick: |_| {
-                        window.send(CoreCommand::clear_completed());
-                    },
-                    "Clear completed"
-                }
+            }
+
+            ul {
+                // doesn't work with dioxus <= v0.2.4 but fix is already merged in master.
+                style: "max-width: 400px; width: 100vw; list-style-type: none;",
+                onmouseleave: |_| {
+                    hovered.set(None);
+                },
+                todo_list.iter().map(|todo| rsx! {
+                    li {
+                        style: "display: flex; align-items: center; justify-content: space-between; background: #ddd; padding: 1rem; height: 32px;",
+                        onmouseover: |_| {
+                            hovered.set(Some(todo.entity));
+                        },
+                        div { style: "display: flex; align-items: center;",
+                            div {
+                                style: "padding-right: 1rem;",
+                                onclick: |_| {
+                                    window.send(CoreCommand::toggle_done(&todo.entity));
+                                },
+                                [format_args!("{}", if todo.done_at.is_some() { "✅" } else { "❎" })],
+                            }
+                            div {
+                                "{todo.title}"
+                            }
+                        }
+
+                        if let Some(entity) = hovered.get() {
+                            if entity == &todo.entity {
+                                cx.render(rsx! {
+                                    button {
+                                        style: "align-self: flex-end",
+                                        onclick: |_| {
+                                            window.send(CoreCommand::remove_todo(&todo.entity));
+                                        },
+                                        "X"
+                                    }
+                                })
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    }
+                })
             }
         }
     })
