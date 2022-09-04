b333c879f35b226295cd3ea4cb931db39c7093ce
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-09-04 11:11:47 +0200

    partially style todomvc with tailwind

--- examples/todomvc/tailwind.config.js
+++ examples/todomvc/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         text: getColor("text"),
         background: getColor("background"),
 
-        accent: getColorVariant("accent", ["DEFAULT", "background"]),
+        accent: getColorVariant("accent", ["DEFAULT"]),
         neutral: getColorVariant("neutral", [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]),
     },
     fontFamily: {
