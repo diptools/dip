e2cace8c6df01ccd059b198b49976b52efe1ea83
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2022-09-04 14:21:01 +0200

    Configure Tailwind for TodoMVC example (#75)
    * configure tailwind for todomvc example
    
    * partially style todomvc with tailwind
    
    * comment about tailwind script

---
+++ examples/todomvc/tailwind.config.js
@@ -0,0 +1,50 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./examples/todomvc/**/*.rs"
+  ],
+  theme: {
+    colors: {
+        inherit: colors.inherit,
+        current: colors.current,
+        transparent: colors.transparent,
+
+        text: getColor("text"),
+        background: getColor("background"),
+
+        accent: getColorVariant("accent", ["DEFAULT"]),
+        neutral: getColorVariant("neutral", [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]),
+    },
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      mono: defaultTheme.fontFamily.mono,
+    },
+    screens: {
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px"
+    },
+    extend: {
+      maxWidth: theme => ({
+        "screen-xl": theme("screens.xl"),
+      })
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
+
+function getColor(name) {
+  return `rgb(var(--color-${name}) / <alpha-value>)`
+}
+
+function getColorVariant(name, keys) {
+  return keys.reduce((prev, key) => ({
+    ...prev,
+    [key]: getColor(key === "DEFAULT" ? name : `${name}-${key}`),
+  }), {});
+}
--- examples/todomvc/ui.rs
+++ examples/todomvc/ui.rs
@@ -13,15 +13,76 @@ pub fn Root(cx: Scope) -> Element {
 
     cx.render(rsx! {
         main {
-            style: "display: flex; flex-direction: column; align-items: center;",
-            h1 { "todos" }
+            class: "w-screen h-screen flex flex-col items-center bg-background",
+            header {
+                class: "w-96 flex flex-col items-center",
+                h1 {
+                    class: "text-8xl text-accent opacity-20 font-thin p-4",
+                    "todos"
+                }
+
+                input {
+                    class: "w-full text-2xl p-1 pl-16",
+                    value: "{new_todo}",
+                    oninput: |e| {
+                        new_todo.set(e.value.clone());
+                    },
+                    onchange: |e| {
+                        window.send(UiAction::create_todo(&e.value));
+                        new_todo.set("".to_string());
+                    }
+                }
+            }
+
+            ul {
+                style: "w-96 flex flex-col items-stretch",
+                // doesn't work with dioxus <= v0.2.4 but fix is already merged in master.
+                onmouseleave: |_| {
+                    hovered.set(None);
+                },
+                todo_list.iter().map(|todo| rsx! {
+                    li {
+                        key: "{todo.entity:?}",
+                        class: "text-2xl p-1 pl-16 flex",
+                        onmouseover: |_| {
+                            hovered.set(Some(todo.entity));
+                        },
+                        div {
+                            onclick: |_| {
+                                window.send(UiAction::toggle_done(&todo.entity));
+                            },
+                            [format_args!("{}", if todo.done_at.is_some() { "✅" } else { "❎" })],
+                        }
+                        input {
+                            value: "{todo.title}",
+                            oninput: |e| {
+                                window.send(UiAction::change_title(&todo.entity, &e.value));
+                            }
+                        }
+
+                        if let Some(entity) = hovered.get() {
+                            if entity == &todo.entity {
+                                cx.render(rsx! {
+                                    button {
+                                        onclick: |_| {
+                                            window.send(UiAction::remove_todo(&todo.entity));
+                                        },
+                                        "X"
+                                    }
+                                })
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    }
+                })
+            }
 
             div {
-                style: "display: flex; flex-direction: column; align-items: center;",
                 div {
-                    style: "display: flex;",
                     label {
-                        style: "margin-right: 0.25rem;",
                         r#for: "filter-select",
                         "Choose a filter:"
                     }
@@ -51,7 +112,6 @@ pub fn Root(cx: Scope) -> Element {
                 }
 
                 ul {
-                    style: "display: flex; list-style-type: none; padding: 0;",
                     li {
                         button {
                             onclick: |_| {
@@ -67,67 +127,6 @@ pub fn Root(cx: Scope) -> Element {
                         }
                     }
                 }
-            }
-
-            input {
-                value: "{new_todo}",
-                oninput: |e| {
-                    new_todo.set(e.value.clone());
-                },
-                onchange: |e| {
-                    window.send(UiAction::create_todo(&e.value));
-                    new_todo.set("".to_string());
-                }
-            }
-
-            ul {
-                // doesn't work with dioxus <= v0.2.4 but fix is already merged in master.
-                style: "max-width: 400px; width: 100vw; list-style-type: none;",
-                onmouseleave: |_| {
-                    hovered.set(None);
-                },
-                todo_list.iter().map(|todo| rsx! {
-                    li {
-                        key: "{todo.entity:?}",
-                        style: "display: flex; align-items: center; justify-content: space-between; background: #ddd; padding: 1rem; height: 32px;",
-                        onmouseover: |_| {
-                            hovered.set(Some(todo.entity));
-                        },
-                        div { style: "display: flex; align-items: center;",
-                            div {
-                                style: "padding-right: 1rem;",
-                                onclick: |_| {
-                                    window.send(UiAction::toggle_done(&todo.entity));
-                                },
-                                [format_args!("{}", if todo.done_at.is_some() { "✅" } else { "❎" })],
-                            }
-                            input {
-                                value: "{todo.title}",
-                                oninput: |e| {
-                                    window.send(UiAction::change_title(&todo.entity, &e.value));
-                                }
-                            }
-                        }
-
-                        if let Some(entity) = hovered.get() {
-                            if entity == &todo.entity {
-                                cx.render(rsx! {
-                                    button {
-                                        style: "align-self: flex-end",
-                                        onclick: |_| {
-                                            window.send(UiAction::remove_todo(&todo.entity));
-                                        },
-                                        "X"
-                                    }
-                                })
-                            } else {
-                                None
-                            }
-                        } else {
-                            None
-                        }
-                    }
-                })
             }
         }
     })
