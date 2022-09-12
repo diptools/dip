5efe4c0e51e547e468f24213020a7c29e89c3c22
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2022-09-12 19:36:34 +0200

    Customizable Cli name (#109)
    * only add subcommand plugin when subcommands exist
    
    * support custom cli struct name

--- packages/macro/src/cli.rs
+++ packages/macro/src/cli.rs
@@ -1,6 +1,8 @@
+use convert_case::{Case, Casing};
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
+use proc_macro2::{TokenStream as TokenStream2, TokenTree};
 use quote::quote;
+use std::str::FromStr;
 use syn::{Fields, ItemEnum, ItemStruct, Variant};
 
 pub struct CliParser {
@@ -15,8 +17,39 @@ impl CliParser {
     pub fn parse(&self) -> CliTokenStreams {
         let cli_name = &self.cli_struct.ident;
 
+        let mut subcommand_resource = quote! {};
+        let mut subcommand_plugin = quote! {};
+
+        for f in self.cli_struct.fields.iter() {
+            for a in f.attrs.iter() {
+                for t in a.tokens.clone().into_iter() {
+                    match t {
+                        TokenTree::Group(g) => {
+                            for s in g.stream() {
+                                match s {
+                                    TokenTree::Ident(ident) => {
+                                        if ident.to_string() == "subcommand" {
+                                            let subcommand_name = f.ident.as_ref().unwrap();
+
+                                            subcommand_resource = quote! { .insert_resource(cli.#subcommand_name.clone()) };
+                                            subcommand_plugin =
+                                                quote! { .add_plugin(SubcommandPlugin) };
+                                        }
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
         CliTokenStreams {
             cli_name: quote! { #cli_name },
+            subcommand_resource,
+            subcommand_plugin,
         }
     }
 }
@@ -24,11 +57,18 @@ impl CliParser {
 #[derive(Default)]
 pub struct CliTokenStreams {
     cli_name: TokenStream2,
+    subcommand_resource: TokenStream2,
+    subcommand_plugin: TokenStream2,
 }
 
 impl CliTokenStreams {
     pub fn gen(&self) -> TokenStream {
-        let Self { cli_name } = self;
+        let Self {
+            cli_name,
+            subcommand_resource,
+            subcommand_plugin,
+        } = self;
+
         let gen = quote! {
             use ::clap::Parser;
 
@@ -36,8 +76,15 @@ impl CliTokenStreams {
 
             impl ::bevy::app::Plugin for CliPlugin {
                 fn build(&self, app: &mut ::bevy::app::App) {
-                    app.insert_resource(#cli_name::parse())
-                        .add_plugin(SubcommandPlugin);
+                    let cli = #cli_name::parse();
+
+                    app.add_plugin(::dip::core::schedule::UiSchedulePlugin)
+                        #subcommand_plugin
+                        #subcommand_resource
+                        .insert_resource(cli)
+                        .set_runner(|mut app| {
+                            app.update();
+                        });
                 }
             }
         };
@@ -57,14 +104,21 @@ impl SubcommandParser {
 
     pub fn parse(&self) -> SubcommandTokenStreams {
         let mut tokens = SubcommandTokenStreams::default();
+        tokens.subcommand_ty_name = self.subcommand_ty_name();
 
         for v in &self.commands_enum.variants {
             tokens.events.push(Self::event(&v));
             tokens.add_events.push(Self::add_event(&v));
+            tokens.event_readers.push(Self::event_reader(&v));
             tokens.handlers.push(Self::handler(&v));
         }
 
         tokens
+    }
+
+    fn subcommand_ty_name(&self) -> TokenStream2 {
+        let ident = &self.commands_enum.ident;
+        quote! { #ident }
     }
 
     fn event(v: &Variant) -> TokenStream2 {
@@ -94,8 +148,18 @@ impl SubcommandParser {
         quote! { .add_event::<#ident>() }
     }
 
+    fn event_reader(v: &Variant) -> TokenStream2 {
+        let ident = &v.ident;
+        let event_name_snake =
+            TokenStream2::from_str(&ident.to_string().to_case(Case::Snake)).unwrap();
+
+        quote! { mut #event_name_snake: ::bevy::ecs::event::EventWriter<#ident>, }
+    }
+
     fn handler(v: &Variant) -> TokenStream2 {
         let ident = &v.ident;
+        let event_name_snake =
+            TokenStream2::from_str(&ident.to_string().to_case(Case::Snake)).unwrap();
 
         match &v.fields {
             Fields::Named(fields) => {
@@ -106,27 +170,21 @@ impl SubcommandParser {
 
                 quote! {
                     Commands::#ident { #(#names)*, } => {
-                        app.world
-                            .get_resource_mut::<::bevy::ecs::event::Events<#ident>>()
-                            .unwrap().send(#ident { #(#names)*, });
+                        #event_name_snake.send(#ident { #(#names)*, });
                     }
                 }
             }
             Fields::Unnamed(_f) => {
                 quote! {
                     Commands::#ident(x) => {
-                        app.world
-                            .get_resource_mut::<::bevy::ecs::event::Events<#ident>>()
-                            .unwrap().send(x.clone());
+                        #event_name_snake.send(x.clone());
                     }
                 }
             }
             Fields::Unit => {
                 quote! {
                     Commands::#ident => {
-                        app.world
-                            .get_resource_mut::<::bevy::ecs::event::Events<#ident>>()
-                            .unwrap().send(#ident);
+                        #event_name_snake.send(#ident);
                     }
                 }
             }
@@ -136,16 +194,20 @@ impl SubcommandParser {
 
 #[derive(Default)]
 pub struct SubcommandTokenStreams {
+    subcommand_ty_name: TokenStream2,
     events: Vec<TokenStream2>,
     add_events: Vec<TokenStream2>,
+    event_readers: Vec<TokenStream2>,
     handlers: Vec<TokenStream2>,
 }
 
 impl SubcommandTokenStreams {
     pub fn gen(&self) -> TokenStream {
         let Self {
+            subcommand_ty_name,
             events,
             add_events,
+            event_readers,
             handlers,
         } = self;
 
@@ -156,21 +218,17 @@ impl SubcommandTokenStreams {
 
             impl ::bevy::app::Plugin for SubcommandPlugin {
                 fn build(&self, app: &mut ::bevy::app::App) {
-                    app.insert_resource(DipCli::parse())
-                        #(#add_events)*
-                        .set_runner(|app| Self::runner(app));
+                    app #(#add_events)*
+                        .add_system_to_stage(::dip::core::schedule::UiStage::Action, handle_subcommand);
                 }
             }
 
-            impl SubcommandPlugin {
-                fn runner(mut app: ::bevy::app::App) {
-                    let cli = app.world.get_resource::<DipCli>().unwrap();
-
-                    match cli.command.clone() {
-                        #(#handlers)*
-                    }
-
-                    app.update();
+            fn handle_subcommand(
+                subcommand: Res<#subcommand_ty_name>,
+                #(#event_readers)*
+            ) {
+                match subcommand.clone() {
+                    #(#handlers)*
                 }
             }
         };
