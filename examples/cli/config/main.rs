7df4bcd1a202693947bb0e3de727ca386df73df8
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-09-26 15:02:51 +0200

    cleanup

--- examples/cli/config/main.rs
+++ examples/cli/config/main.rs
@@ -12,18 +12,8 @@ fn log_config(config: Res<Config>) {
     println!("{:#?}", *config);
 }
 
+#[config_plugin]
 #[derive(Debug, Deserialize)]
-#[config_plugin(
-    env_prefix = "APP", // BACKEND__API_KEY -> APP__BACKEND__API_KEY
-    env_separator = "___", // BACKEND___API_KEY -> BACKEND___API_KEY
-    // defualt look up paths for user_config are
-    // ./{CARGO_PKG_NAME}
-    // $HOME/.{CARGO_PKG_NAME}
-    // $HOME/.config/{CARGO_PKG_NAME}/{CARGO_PKG_NAME}
-    // $HOME/.config/{CARGO_PKG_NAME}
-    override_user_config_path = "examples/cli/config/config/development",
-    override_user_config_path = "path/to/user/config/file2"
-)]
 #[allow(dead_code)]
 struct Config {
     base_url: String,
