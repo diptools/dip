c81d477aa39fc39f67d9e1c5274d22601cf49dff
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-10-01 18:45:25 +0200

    cleanup

--- examples/cli/async.rs
+++ examples/cli/async.rs
@@ -1,17 +1,17 @@
 use dip::prelude::*;
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 use tokio::{runtime::Runtime, sync::mpsc};
 
 fn main() {
     App::new()
         .set_runner(|mut app| {
-            let (tx, mut rx) = mpsc::channel::<FetchResult>(8);
+            let (tx, mut rx) = mpsc::channel::<Response>(8);
             let task_pool = AsyncTaskPool::new(tx.clone());
 
             app.world.insert_resource(task_pool);
             app.update();
 
-            let mut event = app.world.get_resource_mut::<Events<FetchResult>>().unwrap();
+            let mut event = app.world.get_resource_mut::<Events<Response>>().unwrap();
             Runtime::new().unwrap().block_on(async move {
                 while let Some(res) = rx.recv().await {
                     event.send(res);
@@ -22,25 +22,30 @@ fn main() {
 
             app.update();
         })
-        .add_event::<FetchResult>()
+        .add_event::<Response>()
         .add_startup_system(fetch)
         .add_system(log_fetch_result)
         .run();
 }
 
-pub struct AsyncTaskPool {
+pub struct AsyncTaskPool<Response> {
     runner: Runtime,
-    tx: mpsc::Sender<FetchResult>,
+    tx: mpsc::Sender<Response>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct IpResponse {
+    origin: String,
 }
 
 #[allow(dead_code)]
 #[derive(Clone, Debug)]
-struct FetchResult {
-    value: HashMap<String, String>,
+enum Response {
+    IpResponse(IpResponse),
 }
 
-impl AsyncTaskPool {
-    fn new(tx: mpsc::Sender<FetchResult>) -> Self {
+impl<Response> AsyncTaskPool<Response> {
+    fn new(tx: mpsc::Sender<Response>) -> Self {
         Self {
             runner: Runtime::new().unwrap(),
             tx,
@@ -48,23 +53,27 @@ impl AsyncTaskPool {
     }
 }
 
-fn fetch(task_pool: Res<AsyncTaskPool>) {
+fn fetch(task_pool: Res<AsyncTaskPool<Response>>) {
     let tx = task_pool.tx.clone();
 
     task_pool.runner.spawn(async move {
         let res = reqwest::get("https://httpbin.org/ip")
             .await
             .unwrap()
-            .json::<HashMap<String, String>>()
+            .json::<IpResponse>()
             .await
             .unwrap();
 
-        tx.send(FetchResult { value: res }).await.unwrap();
+        tx.send(Response::IpResponse(res)).await.unwrap();
     });
 }
 
-fn log_fetch_result(mut events: EventReader<FetchResult>) {
+fn log_fetch_result(mut events: EventReader<Response>) {
     for res in events.iter() {
-        println!("{res:#?}");
+        match res {
+            Response::IpResponse(res) => {
+                println!("{res:#?}");
+            }
+        }
     }
 }
