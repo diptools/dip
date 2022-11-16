b4d20d8dadbf43576332564e966036bc5e7f7f4a
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-11-16 09:08:10 +0100

    Create installs directory when it does not exist

--- plugins/bundle/src/config.rs
+++ plugins/bundle/src/config.rs
@@ -8,6 +8,7 @@ use config::{
 };
 use dip_core::config::{build_config, ConfigPlugin as ConfigPluginRaw};
 use serde::Deserialize;
+use std::{collections::HashSet, fs, path::PathBuf};
 
 pub struct BundleConfigPlugin;
 
@@ -21,33 +22,115 @@ impl Plugin for BundleConfigPlugin {
 }
 
 fn add_sources(mut builder: ResMut<ConfigBuilder<DefaultState>>) {
-    let dip_global_config_path = dirs::data_dir().unwrap().join("dip");
-    let user_bundle_config_path = dip_global_config_path.join("bundle.toml");
+    let config_file = BundleConfig::config_file();
 
-    match user_bundle_config_path.canonicalize() {
-        Ok(user_bundle_config_path) => {
-            *builder = builder.clone().add_source(File::with_name(
-                &user_bundle_config_path.display().to_string(),
-            ));
+    match config_file.canonicalize() {
+        Ok(p) => {
+            *builder = builder
+                .clone()
+                .add_source(File::with_name(&p.display().to_string()));
         }
         Err(_e) => {
-            eprintln!(
-                "Cannot find dip config file: {}",
-                user_bundle_config_path.display()
-            );
+            eprintln!("Cannot find dip config file: {}", config_file.display());
         }
     }
 }
 
+/// General dip configuration
+// TODO: This struct is not only for bundle feature. Move to somewhere general.
+pub struct Config;
+
+impl Config {
+    pub fn config_dir() -> PathBuf {
+        let p = dirs::data_dir().unwrap().join("dip");
+        Self::ensure_dir(&p);
+
+        p
+    }
+
+    pub fn ensure_dir(p: &PathBuf) {
+        if !&p.is_dir() {
+            fs::create_dir_all(&p).unwrap();
+        }
+    }
+
+    pub fn to_path(value: &String) -> PathBuf {
+        value
+            .replace(
+                "$HOME",
+                dirs::home_dir()
+                    .expect("Cannot find home directory.")
+                    .to_str()
+                    .expect("Failed to convert path to string."),
+            )
+            .replace(
+                "$CONFIG_DIR",
+                dirs::config_dir()
+                    .expect("Cannot find config directory.")
+                    .to_str()
+                    .expect("Failed to convert path to string."),
+            )
+            .into()
+    }
+}
+
 #[derive(Deserialize, Debug, Clone)]
+/// Dip configuration regarding to bundle feature.
 pub struct BundleConfig {
+    /// Path to your local bundle repository.
+    repo: String,
+    /// Section for the Version Manager
     pub vm: VMConfig,
 }
 
-#[derive(Deserialize, Debug, Clone)]
-pub struct VMConfig {
-    pub tailwindcss: VersionList,
-    pub nodejs: VersionList,
+impl BundleConfig {
+    pub fn repo(&self) -> PathBuf {
+        let p = Config::to_path(&self.repo);
+        Config::ensure_dir(&p);
+
+        p
+    }
+
+    pub fn config_file() -> PathBuf {
+        let p = Config::config_dir().join("bundle");
+        Config::ensure_dir(&p);
+
+        p
+    }
+
+    pub fn bundle_dir(&self) -> PathBuf {
+        let p = Config::config_dir().join("bundle");
+        Config::ensure_dir(&p);
+
+        p
+    }
+
+    pub fn installs_dir(&self) -> PathBuf {
+        let p = self.bundle_dir().join("installs");
+        Config::ensure_dir(&p);
+
+        p
+    }
 }
 
-pub type VersionList = Vec<String>;
+#[derive(Deserialize, Debug, Clone)]
+/// Version Manager related configurations
+pub struct VMConfig {
+<<<<<<< HEAD
+    pub tailwindcss: VersionList,
+    pub nodejs: VersionList,
+=======
+    /// All runtime versions
+    pub runtime: VMRuntime,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct VMRuntime {
+    /// [Tailwind CSS](https://tailwindcss.com/)
+    pub tailwindcss: VersionSet,
+    /// [Node.js](https://nodejs.org/)
+    pub nodejs: VersionSet,
+>>>>>>> 051d114 (Create installs directory when it does not exist)
+}
+
+pub type VersionSet = HashSet<String>;
--- plugins/bundle/src/tool/vm/tailwind.rs
+++ plugins/bundle/src/tool/vm/tailwind.rs
@@ -1,15 +1,28 @@
+<<<<<<< HEAD
 use crate::tool::InstallTools;
+=======
+use crate::{
+    config::{BundleConfig, Config},
+    schedule::BundleStage,
+    ApplyBundle, CleanBundle,
+};
+>>>>>>> 051d114 (Create installs directory when it does not exist)
 use bevy::{
     app::{App, Plugin},
     ecs::event::{EventReader, EventWriter},
     log,
 };
+<<<<<<< HEAD
+=======
+use std::{fs, path::PathBuf};
+>>>>>>> 051d114 (Create installs directory when it does not exist)
 
 // Plugin
 pub struct TailwindPlugin;
 
 impl Plugin for TailwindPlugin {
     fn build(&self, app: &mut App) {
+<<<<<<< HEAD
         app.add_event::<TailwindInstalled>().add_system(install);
     }
 }
@@ -19,6 +32,62 @@ fn install(mut events: EventReader<InstallTools>, mut installed: EventWriter<Tai
         log::warn!("TODO: Install Tool");
 
         installed.send(TailwindInstalled);
+=======
+        app.add_system_to_stage(BundleStage::Clean, clean)
+            .add_system_to_stage(BundleStage::Apply, apply);
+    }
+}
+
+fn clean(mut events: EventReader<ApplyBundle>, config: Res<BundleConfig>) {
+    events.iter().for_each(|_e| {
+        log::warn!("TODO: Implement clean system for Version Manager");
+        let tw = TailwindCSS::new(config.clone());
+
+        if let Ok(installs_dir) = tw.installs_dir().canonicalize() {
+            let installs = fs::read_dir(installs_dir).expect("Failed to read installs/ directory");
+
+            // iterate over runtimes x versions currently installed but removed from the user bundle config
+            installs.for_each(|dir| {
+                println!("{dir:?}");
+            });
+        } else {
+            println!("Tailwind CSS is not installed");
+        }
+    });
+}
+
+fn apply(mut events: EventReader<ApplyBundle>, config: Res<BundleConfig>) {
+    events.iter().for_each(|_e| {
+        log::warn!("TODO: Implement install system for Version Manager");
+
+        let tw = TailwindCSS::new(config.clone());
+        log::info!("{:#?}", tw.config);
+        tw.versions().for_each(|v| {
+            log::info!("{v}");
+        });
+    });
+}
+
+struct TailwindCSS {
+    config: BundleConfig,
+}
+
+impl TailwindCSS {
+    fn new(config: BundleConfig) -> Self {
+        Self { config }
+    }
+
+    fn bundle_dir(&self) -> PathBuf {
+        self.config.repo().join("bundle/vm")
+    }
+
+    fn installs_dir(&self) -> PathBuf {
+        self.config.installs_dir().join("tailwindcss")
+    }
+
+    fn versions(&self) -> std::collections::hash_set::Iter<'_, std::string::String> {
+        self.config.vm.runtime.tailwindcss.iter()
+>>>>>>> 051d114 (Create installs directory when it does not exist)
     }
 }
 
